92692546613650ab6d133842aacbf7e26c614f50
Mark J. Williamson <mw529@cam.ac.uk>
2015-11-17 13:55:51 +0000

    Add in an electron density calculation example

---
+++ src/test/java/name/mjw/jquante/math/qm/property/ElectronDensityTest.java
@@ -0,0 +1,72 @@
+package name.mjw.jquante.math.qm.property;
+
+import static org.junit.Assert.assertEquals;
+import name.mjw.jquante.math.geom.Point3D;
+import name.mjw.jquante.math.qm.BasisFunctions;
+import name.mjw.jquante.math.qm.OneElectronIntegrals;
+import name.mjw.jquante.math.qm.SCFMethod;
+import name.mjw.jquante.math.qm.SCFMethodFactory;
+import name.mjw.jquante.math.qm.SCFType;
+import name.mjw.jquante.math.qm.TwoElectronIntegrals;
+import name.mjw.jquante.molecule.Atom;
+import name.mjw.jquante.molecule.Molecule;
+import name.mjw.jquante.molecule.impl.MoleculeImpl;
+import name.mjw.jquante.molecule.property.electronic.GridProperty;
+
+import org.junit.Test;
+
+public class ElectronDensityTest {
+
+	double diff = 0.00001;
+
+	@Test
+	public void test() throws Exception {
+		// Create a molecule of water
+		Atom O = new Atom("O", 6.0, new Point3D(0.0000000, 0.000000, 0.119748));
+		Atom H1 = new Atom("H", 1.0, new Point3D(0.00000000, 0.761561,
+				-0.478993));
+		Atom H2 = new Atom("H", 1.0, new Point3D(0.00000000, -0.761561,
+				-0.478993));
+
+		Molecule water = new MoleculeImpl("water");
+		water.addAtom(H1);
+		water.addAtom(O);
+		water.addAtom(H2);
+
+		// Read Basis
+		BasisFunctions basisFunctions = new BasisFunctions(water, "sto3g");
+
+		// Compute integrals
+		OneElectronIntegrals oneElectronIntegrals = new OneElectronIntegrals(
+				basisFunctions, water);
+		TwoElectronIntegrals twoElectronIntegrals = new TwoElectronIntegrals(
+				basisFunctions);
+
+		// Set up HF method
+		SCFMethod scfm = SCFMethodFactory.getInstance().getSCFMethod(water,
+				oneElectronIntegrals, twoElectronIntegrals,
+				SCFType.HARTREE_FOCK);
+
+		// Actually do the SCF
+		scfm.scf();
+
+		// Set up the grid for where the electron density will be calculated:
+		//
+		// Set 5 grid points per x,y,z direction.
+		// Extend the bounding box by 2 units in each direction.
+		GridProperty gridProperty = new GridProperty(water.getBoundingBox()
+				.expand(2.0), 5);
+
+		// Computes electron density on specified points for a Molecule
+		ElectronDensity electronDensity = new ElectronDensity(scfm);
+		electronDensity.compute(gridProperty);
+
+		// Check the a few points
+		assertEquals(0.08352817419051489,
+				gridProperty.getFunctionValueAt(2, 2, 2), diff);
+
+		assertEquals(0.1849781416342963,
+				gridProperty.getFunctionValueAt(2, 2, 3), diff);
+
+	}
+}
