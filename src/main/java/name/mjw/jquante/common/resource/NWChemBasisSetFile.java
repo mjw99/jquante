41990f727ce2952e27a065c792f36e2ac04dbad8
Mark J. Williamson <mw529@cam.ac.uk>
2017-07-20 13:49:28 +0100

    Add support for Pople SP-shell parsing

--- src/main/java/name/mjw/jquante/common/resource/NWChemBasisSetFile.java
+++ src/main/java/name/mjw/jquante/common/resource/NWChemBasisSetFile.java
@@ -1,6 +1,7 @@
 package name.mjw.jquante.common.resource;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -33,11 +34,13 @@ public class NWChemBasisSetFile {
 	private final static Logger LOG = Logger
 			.getLogger(NWChemBasisSetFile.class);
 
+	private final static int markLength = 5000;
+
 	@XmlAttribute(name = "name")
 	private String basisSetName;
 
 	@XmlAttribute(name = "md5sum")
-	private String md5sum;
+	private String md5sumOfInputBasisFile;
 
 	@XmlElement(name = "atom")
 	private List<Library> atoms;
@@ -48,7 +51,9 @@ public class NWChemBasisSetFile {
 	 * 
 	 * @see <a
 	 *      href="https://github.com/jeffhammond/nwchem/blob/HEAD/src/basis/basis_dox.c">src/basis/basis_dox.c</a>
-	 * 
+	 * @see <a
+	 *      href="http://www.ccl.net/cca/documents/basis-sets/basis.html">General
+	 *      basis set naming information</a>
 	 * 
 	 */
 	public NWChemBasisSetFile() {
@@ -71,7 +76,8 @@ public class NWChemBasisSetFile {
 	 */
 	public void read(String fileName) {
 
-		basisSetName = fileName;
+		File f = new File(fileName);
+		basisSetName = f.getName();
 		MessageDigest md = null;
 
 		try {
@@ -99,7 +105,8 @@ public class NWChemBasisSetFile {
 			}
 
 			byte[] digest = md.digest();
-			md5sum = DatatypeConverter.printHexBinary(digest).toLowerCase();
+			md5sumOfInputBasisFile = DatatypeConverter.printHexBinary(digest)
+					.toLowerCase();
 
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
@@ -146,7 +153,7 @@ public class NWChemBasisSetFile {
 
 		List<Shell> shells = new ArrayList<Shell>();
 
-		br.mark(5000);
+		br.mark(markLength);
 		while ((line = br.readLine()) != null) {
 
 			if (line.matches("^end.*")) {
@@ -164,7 +171,7 @@ public class NWChemBasisSetFile {
 
 			}
 
-			br.mark(5000);
+			br.mark(markLength);
 
 		}
 
@@ -197,12 +204,12 @@ public class NWChemBasisSetFile {
 		String line;
 		ArrayList<Shell> shells = null;
 		ArrayList<Object> lineObjects;
-		final String orbitalFormat = "(1X, E18.7, 9X, 6(E15.7, 8X))";
+		final String shellFormat = "(1X, E18.7, 9X, 6(E15.7, 8X))";
 
 		DecimalFormat df = new DecimalFormat("0",
 				DecimalFormatSymbols.getInstance(Locale.ENGLISH));
 		df.setMaximumFractionDigits(340); // 340 =
-											// DecimalFormat.DOUBLE_FRACTION_DIGITS
+		// DecimalFormat.DOUBLE_FRACTION_DIGITS
 
 		line = br.readLine();
 		LOG.debug("");
@@ -213,23 +220,34 @@ public class NWChemBasisSetFile {
 
 		String currentElement = (String) lineObjects.get(0);
 		String shellType = (String) lineObjects.get(1);
-		// TODO check for a SP shell
 
 		// Peek ahead here to work out how many contractedShells to return
-		br.mark(5000);
+		br.mark(markLength);
 		line = br.readLine();
 
 		// Hack since FortranFormat does not
 		// understand Double-Precision Exponent
 		line = line.replace("D", "E");
-		lineObjects = FortranFormat.read(line, orbitalFormat);
+		lineObjects = FortranFormat.read(line, shellFormat);
 		numberOfContractedShellsToParse = getNonNullLength(lineObjects) - 1;
 		br.reset();
 
 		shells = new ArrayList<Shell>(numberOfContractedShellsToParse);
 
-		for (int i = 0; i < numberOfContractedShellsToParse; i++) {
-			shells.add(new Shell(shellType));
+		// For the basis sets in which s- and p-type functions share the same
+		// exponents, the term SP-shell is used.
+		if (shellType.equals("SP")) {
+			LOG.debug("SP Found");
+
+			for (int i = 0; i < numberOfContractedShellsToParse; i++) {
+				LOG.debug(ShellType.values()[i]);
+				shells.add(new Shell(ShellType.values()[i].toString()));
+
+			}
+		} else {
+			for (int i = 0; i < numberOfContractedShellsToParse; i++) {
+				shells.add(new Shell(shellType));
+			}
 		}
 
 		while (true) {
@@ -249,7 +267,7 @@ public class NWChemBasisSetFile {
 			// Exponent
 			line = line.replace("D", "E");
 
-			lineObjects = FortranFormat.read(line, orbitalFormat);
+			lineObjects = FortranFormat.read(line, shellFormat);
 			LOG.debug(lineObjects);
 
 			exp = (Double) lineObjects.get(0);
@@ -262,7 +280,7 @@ public class NWChemBasisSetFile {
 						df.format(coeff));
 			}
 
-			br.mark(5000);
+			br.mark(markLength);
 		}
 
 	}
@@ -340,5 +358,15 @@ public class NWChemBasisSetFile {
 			if (el != null)
 				++count;
 		return count;
+	}
+
+	private enum ShellType {
+		S(0), P(1), D(2), F(3), G(4), H(5);
+
+		private int value;
+
+		private ShellType(int value) {
+			this.value = value;
+		}
 	}
 }
