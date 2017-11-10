7477ce4f554a512cc35bf7d2702917c07242b4a3
Mark J. Williamson <mw529@cam.ac.uk>
2017-11-10 10:54:16 +0000

    Improve test layout and test for problematic power

--- src/test/java/name/mjw/jquante/math/qm/integral/HGPTwoElectronTermTest.java
+++ src/test/java/name/mjw/jquante/math/qm/integral/HGPTwoElectronTermTest.java
@@ -1,6 +1,9 @@
 package name.mjw.jquante.math.qm.integral;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
 import name.mjw.jquante.math.geom.Point3D;
 import name.mjw.jquante.math.qm.basis.ContractedGaussian;
 import name.mjw.jquante.math.qm.basis.Power;
@@ -10,36 +13,40 @@ import org.junit.Test;
 public class HGPTwoElectronTermTest {
 	private final double delta = 0.000001;
 
-	@Test
-	public void testOne() {
+	HGPTwoElectronTerm e2 = new HGPTwoElectronTerm();
 
-		ContractedGaussian cgto1 = new ContractedGaussian(new Point3D(0, 0, 0),
-				new Power(0, 0, 0));
-		cgto1.addPrimitive(1.0, 1.0);
-		cgto1.normalize();
+	ContractedGaussian cgtoS0;
+	ContractedGaussian cgtoS1;
+	ContractedGaussian cgtoP0;
 
-		HGPTwoElectronTerm e2 = new HGPTwoElectronTerm();
+	@Before
+	public void setUp() {
 
-		assertEquals(1.1283791633342477,
-				e2.coulomb(cgto1, cgto1, cgto1, cgto1), delta);
+		cgtoS0 = new ContractedGaussian(new Point3D(0, 0, 0), new Power(0, 0, 0));
+		cgtoS0.addPrimitive(1.0, 1.0);
+		cgtoS0.normalize();
+
+		cgtoS1 = new ContractedGaussian(new Point3D(0, 0, 1), new Power(0, 0, 0));
+		cgtoS1.addPrimitive(1.0, 1.0);
+		cgtoS1.normalize();
+
+		cgtoP0 = new ContractedGaussian(new Point3D(0, 0, 0), new Power(1, 0, 0));
+		cgtoP0.addPrimitive(1.0, 1.0);
+		cgtoP0.normalize();
 	}
 
 	@Test
-	public void testTwo() {
+	public void testSS00() {
+		assertEquals(1.1283791633342477, e2.coulomb(cgtoS0, cgtoS0, cgtoS0, cgtoS0), delta);
+	}
 
-		ContractedGaussian cgto1 = new ContractedGaussian(new Point3D(0, 0, 0),
-				new Power(0, 0, 0));
-		cgto1.addPrimitive(1.0, 1.0);
-		cgto1.normalize();
+	@Test
+	public void testSS01() {
+		assertEquals(0.8427007900292194, e2.coulomb(cgtoS0, cgtoS0, cgtoS1, cgtoS1), delta);
+	}
 
-		ContractedGaussian cgto2 = new ContractedGaussian(new Point3D(0, 0, 1),
-				new Power(0, 0, 0));
-		cgto2.addPrimitive(1.0, 1.0);
-		cgto2.normalize();
-
-		HGPTwoElectronTerm e2 = new HGPTwoElectronTerm();
-
-		assertEquals(0.8427007900292194,
-				e2.coulomb(cgto1, cgto1, cgto2, cgto2), delta);
+	@Test
+	public void testSP00() {
+		assertEquals(0.9403159699467084, e2.coulomb(cgtoS0, cgtoS0, cgtoP0, cgtoP0), delta);
 	}
 }
