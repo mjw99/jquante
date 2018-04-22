9ab2fc1bfc4a018db5553a3c5c08a1ede4ae72ae
Mark Williamson <mjw@mjw.name>
2018-04-22 11:42:38 +0100

    Add unit tests for findDihedral()

--- src/test/java/name/mjw/jquante/math/MathUtilTest.java
+++ src/test/java/name/mjw/jquante/math/MathUtilTest.java
@@ -2,6 +2,7 @@ package name.mjw.jquante.math;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.junit.Test;
 
 public class MathUtilTest {
@@ -65,6 +66,30 @@ public class MathUtilTest {
 	@Test
 	public void radiansToDegrees() {
 		assertEquals(Math.PI / 4, MathUtil.toRadians(45), diff);
+
+	}
+
+	@Test
+	public void testfindDihedralGetDihedralAngle1() {
+
+		Vector3D i = new Vector3D(60.614941, 67.371346, 12.713049);
+		Vector3D j = new Vector3D(59.104156, 67.044731, 12.827555);
+		Vector3D k = new Vector3D(58.827194, 65.911911, 13.835642);
+		Vector3D l = new Vector3D(59.367950, 66.384277, 15.233553);
+
+		assertEquals(59.66383, MathUtil.toDegrees(MathUtil.findDihedral(i, j, k, l)), diff);
+
+	}
+
+	@Test
+	public void testfindDihedralGetDihedralAngle2() {
+
+		Vector3D i = new Vector3D(30.958572, 73.709137, 38.031029);
+		Vector3D j = new Vector3D(31.924915, 72.577698, 37.985279);
+		Vector3D k = new Vector3D(31.643818, 71.563255, 36.873047);
+		Vector3D l = new Vector3D(31.253489, 70.131218, 37.371773);
+
+		assertEquals(-115.172063, MathUtil.toDegrees(MathUtil.findDihedral(i, j, k, l)), diff);
 
 	}
 
