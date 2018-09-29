b619f90fef7124396b9c90c52ba539438a956d03
Mark J. Williamson <mjw@mjw.name>
2018-09-29 15:15:38 +0100

    Initial support for Lebedev integration

---
+++ src/main/java/name/mjw/jquante/math/LebedevGridPoint.java
@@ -0,0 +1,109 @@
+package name.mjw.jquante.math;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
+
+import net.jafama.FastMath;
+
+/**
+ * Point information on a Lebedev grid.
+ */
+public class LebedevGridPoint {
+
+	/** x coordinate of the point in the unit sphere. */
+	private final double x;
+	/** y coordinate of the point in the unit sphere. */
+	private final double y;
+	/** z coordinate of the point in the unit sphere. */
+	private final double z;
+	/** Weight of the point. */
+	private final double weight;
+
+	/**
+	 * Constructor (point must be in the unit sphere so x*x + y*x + z*z == 1).
+	 * 
+	 * @param xIn      x coordinate
+	 * @param yIn      y coordinate
+	 * @param zIn      z coordinate
+	 * @param weightIn point weight
+	 */
+	public LebedevGridPoint(final double xIn, final double yIn, final double zIn, final double weightIn) {
+		this.x = xIn;
+		this.y = yIn;
+		this.z = zIn;
+		this.weight = weightIn;
+	}
+
+	/**
+	 * Returns the grid point.
+	 * 
+	 * @return the grid point
+	 */
+	public final Vector3D getVector3D() {
+		return new Vector3D(x, y, z);
+	}
+
+	/**
+	 * Returns the grid point.
+	 * 
+	 * @return the grid point
+	 */
+	public final RealVector getXYZ() {
+		return new ArrayRealVector(new double[] { x, y, z }, false);
+	}
+
+	/**
+	 * Returns the grid point X.
+	 * 
+	 * @return the X grid point
+	 */
+	public final double getX() {
+		return x;
+	}
+
+	/**
+	 * Returns the grid point Y.
+	 * 
+	 * @return the Y grid point
+	 */
+	public final double getY() {
+		return y;
+	}
+
+	/**
+	 * Returns the grid point Z.
+	 * 
+	 * @return the Z grid point
+	 */
+	public final double getZ() {
+		return z;
+	}
+
+	/**
+	 * Returns the grid point's weight.
+	 * 
+	 * @return the grid point's weight
+	 */
+	public final double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Returns the grid point coelevation.
+	 * 
+	 * @return the grid point coelevation in [-PI,PI]
+	 */
+	public double getPhi() {
+		return FastMath.acos(z);
+	}
+
+	/**
+	 * Returns the grid point azimuth.
+	 * 
+	 * @return the grid point azimuth in [-PI,PI]
+	 */
+	public double getTheta() {
+		return FastMath.atan2(y, x);
+	}
+}
---
+++ src/test/java/name/mjw/jquante/math/LebedevRuleTest.java
@@ -0,0 +1,88 @@
+package name.mjw.jquante.math;
+
+import java.util.List;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.util.FastMath;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LebedevRuleTest {
+	@Test
+	public void testSphereIntegration() {
+
+		for (int rule = 1; rule <= LebedevRule.MAX_RULE; rule++) {
+			final List<LebedevGridPoint> grid = LebedevRule.createGridForRule(rule);
+
+			if (grid == null) {
+				Assert.assertEquals(0, LebedevRule.isRuleAvailable(rule));
+			} else {
+				Assert.assertEquals(1, LebedevRule.isRuleAvailable(rule));
+				Assert.assertEquals(LebedevRule.getRuleOrder(rule), grid.size());
+
+				// integrate unitary function
+				double result = 0.;
+				Vector3D centroid = Vector3D.ZERO;
+				for (final LebedevGridPoint point : grid) {
+					result += point.getWeight();
+					centroid = centroid.add(point.getVector3D());
+				}
+				// the integration of 1 should be 1 (the actual integral should be multiplied by
+				// 4pi r^2)
+				Assert.assertEquals(rule + " Prec=" + LebedevRule.getRulePrecision(rule), 1.0, result, 1E-13);
+				// centroid is guaranteed to be zero due to symmetry of grid
+				Assert.assertEquals(0.0, centroid.getNorm(), 1E-14);
+			}
+		}
+
+	}
+
+	@Test
+	public void testGetters() {
+		final LebedevGridPoint point = LebedevRule.createGridForRule(1).get(0);
+		Assert.assertEquals(FastMath.PI / 2., point.getPhi(), 1E-15); // coelevation
+		Assert.assertEquals(0.0, point.getTheta(), 1E-15); // azimuth
+		Assert.assertEquals(1.0, point.getX(), 0.);
+		Assert.assertEquals(0.0, point.getY(), 0.);
+		Assert.assertEquals(0.0, point.getZ(), 0.);
+	}
+
+	@Test
+	public void testLebedevBadOrder() {
+		try {
+			LebedevRule.createGridByOrder(0);
+			Assert.fail();
+		} catch (final IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			LebedevRule.getRuleOrder(0);
+			Assert.fail();
+		} catch (final IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			LebedevRule.getRuleOrder(1000);
+			Assert.fail();
+		} catch (final IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			LebedevRule.getRulePrecision(0);
+			Assert.fail();
+		} catch (final IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			LebedevRule.getRulePrecision(1000);
+			Assert.fail();
+		} catch (final IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+	}
+
+}
