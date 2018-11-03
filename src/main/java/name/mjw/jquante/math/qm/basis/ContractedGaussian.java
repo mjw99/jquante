mjw99/jquante

commit 78f1de6f1138e4bd722e90e7e020f5b6902062bd

Use Collections interface over implementation

src/main/java/name/mjw/jquante/math/qm/basis/ContractedGaussian.java
@@ -1,6 +1,7 @@
 package name.mjw.jquante.math.qm.basis;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import name.mjw.jquante.config.impl.AtomInfo;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
@@ -29,17 +30,17 @@ public class ContractedGaussian {
 	/**
 	 * The Primitive Gaussians (PGs) that make up this Contracted Gaussian.
 	 */
-	private ArrayList<PrimitiveGaussian> primitives;
+	private List<PrimitiveGaussian> primitives;
 
 	/**
 	 * The list of exponents for this Gaussian.
 	 */
-	private ArrayList<Double> exponents;
+	private List<Double> exponents;
 
 	/**
 	 * Holds value of property coefficients.
 	 */
-	private ArrayList<Double> coefficients;
+	private List<Double> coefficients;
 
 	/**
 	 * Holds value of property normalization.
@@ -51,7 +52,7 @@ public class ContractedGaussian {
 	 * 
 	 * normalization factors for PGs
 	 */
-	private ArrayList<Double> primNorms;
+	private List<Double> primNorms;
 
 	protected Atom centeredAtom;
 
@@ -169,7 +170,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property primitives.
 	 */
-	public ArrayList<PrimitiveGaussian> getPrimitives() {
+	public List<PrimitiveGaussian> getPrimitives() {
 		return this.primitives;
 	}
 
@@ -179,7 +180,7 @@ public class ContractedGaussian {
 	 * @param primitives
 	 *            New value of property primitives.
 	 */
-	public void setPrimitives(ArrayList<PrimitiveGaussian> primitives) {
+	public void setPrimitives(List<PrimitiveGaussian> primitives) {
 		this.primitives = primitives;
 	}
 
@@ -188,7 +189,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property exponents.
 	 */
-	public ArrayList<Double> getExponents() {
+	public List<Double> getExponents() {
 		return this.exponents;
 	}
 
@@ -207,7 +208,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property coefficients.
 	 */
-	public ArrayList<Double> getCoefficients() {
+	public List<Double> getCoefficients() {
 		return this.coefficients;
 	}
 
@@ -217,7 +218,7 @@ public class ContractedGaussian {
 	 * @param coefficients
 	 *            New value of property coefficients.
 	 */
-	public void setCoefficients(ArrayList<Double> coefficients) {
+	public void setCoefficients(List<Double> coefficients) {
 		this.coefficients = coefficients;
 	}
 
@@ -263,7 +264,7 @@ public class ContractedGaussian {
 	public double overlap(ContractedGaussian cg) {
 		double sij = 0.0;
 
-		ArrayList<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
+		List<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
 
 		for (int i = 0; i < primitives.size(); i++) {
 			PrimitiveGaussian iPG = primitives.get(i);
@@ -380,7 +381,7 @@ public class ContractedGaussian {
 	 */
 	public double kinetic(ContractedGaussian cg) {
 		double tij = 0.0;
-		ArrayList<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
+		List<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
 
 		for (int i = 0; i < primitives.size(); i++) {
 			PrimitiveGaussian iPG = primitives.get(i);
@@ -505,7 +506,7 @@ public class ContractedGaussian {
 		int i;
 		int j;
 
-		ArrayList<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
+		List<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
 
 		for (i = 0; i < primitives.size(); i++) {
 			PrimitiveGaussian iPG = primitives.get(i);
@@ -714,7 +715,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property primNorms.
 	 */
-	public ArrayList<Double> getPrimNorms() {
+	public List<Double> getPrimNorms() {
 		return this.primNorms;
 	}
 

src/main/java/name/mjw/jquante/math/qm/integral/HGPTwoElectronTerm.java
@@ -1,6 +1,7 @@
 package name.mjw.jquante.math.qm.integral;
 
-import java.util.ArrayList;
+import java.util.List;
+
 import name.mjw.jquante.math.qm.Density;
 import name.mjw.jquante.math.qm.basis.ContractedGaussian;
 import name.mjw.jquante.math.qm.basis.Power;
@@ -98,11 +99,11 @@ public class HGPTwoElectronTerm implements TwoElectronTerm {
 	 * 
 	 * @return Contribution to Horizontal Recurrence Relation.
 	 */
-	protected double contractedHrr(Vector3D a, Power aPower, ArrayList<Double> aCoeff, ArrayList<Double> aExps,
-			ArrayList<Double> aNorms, Vector3D b, Power bPower, ArrayList<Double> bCoeff, ArrayList<Double> bExps,
-			ArrayList<Double> bNorms, Vector3D c, Power cPower, ArrayList<Double> cCoeff, ArrayList<Double> cExps,
-			ArrayList<Double> cNorms, Vector3D d, Power dPower, ArrayList<Double> dCoeff, ArrayList<Double> dExps,
-			ArrayList<Double> dNorms) {
+	protected double contractedHrr(Vector3D a, Power aPower, List<Double> aCoeff, List<Double> aExps,
+			List<Double> aNorms, Vector3D b, Power bPower, List<Double> bCoeff, List<Double> bExps,
+			List<Double> bNorms, Vector3D c, Power cPower, List<Double> cCoeff, List<Double> cExps,
+			List<Double> cNorms, Vector3D d, Power dPower, List<Double> dCoeff, List<Double> dExps,
+			List<Double> dNorms) {
 
 		int la = aPower.getL();
 		int ma = aPower.getM();
@@ -211,11 +212,11 @@ public class HGPTwoElectronTerm implements TwoElectronTerm {
 	 * 
 	 * @return Contribution to Vertical Recurrence Relation.
 	 */
-	protected double contractedVrr(Vector3D a, Power aPower, ArrayList<Double> aCoeff, ArrayList<Double> aExps,
-			ArrayList<Double> aNorms, Vector3D b, ArrayList<Double> bCoeff, ArrayList<Double> bExps,
-			ArrayList<Double> bNorms, Vector3D c, Power cPower, ArrayList<Double> cCoeff, ArrayList<Double> cExps,
-			ArrayList<Double> cNorms, Vector3D d, ArrayList<Double> dCoeff, ArrayList<Double> dExps,
-			ArrayList<Double> dNorms) {
+	protected double contractedVrr(Vector3D a, Power aPower, List<Double> aCoeff, List<Double> aExps,
+			List<Double> aNorms, Vector3D b, List<Double> bCoeff, List<Double> bExps,
+			List<Double> bNorms, Vector3D c, Power cPower, List<Double> cCoeff, List<Double> cExps,
+			List<Double> cNorms, Vector3D d, List<Double> dCoeff, List<Double> dExps,
+			List<Double> dNorms) {
 
 		double value = 0.0;
 

src/main/java/name/mjw/jquante/math/qm/integral/HuzinagaTwoElectronTerm.java
@@ -1,6 +1,6 @@
 package name.mjw.jquante.math.qm.integral;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -49,27 +49,27 @@ public class HuzinagaTwoElectronTerm implements TwoElectronTerm {
 		double kcNorm;
 		double repulsionTerm;
 
-		ArrayList<Double> aExps = a.getExponents();
-		ArrayList<Double> aCoefs = a.getCoefficients();
-		ArrayList<Double> aNorms = a.getPrimNorms();
+		List<Double> aExps = a.getExponents();
+		List<Double> aCoefs = a.getCoefficients();
+		List<Double> aNorms = a.getPrimNorms();
 		Vector3D aOrigin = a.getOrigin();
 		Power aPower = a.getPowers();
 
-		ArrayList<Double> bExps = b.getExponents();
-		ArrayList<Double> bCoefs = b.getCoefficients();
-		ArrayList<Double> bNorms = b.getPrimNorms();
+		List<Double> bExps = b.getExponents();
+		List<Double> bCoefs = b.getCoefficients();
+		List<Double> bNorms = b.getPrimNorms();
 		Vector3D bOrigin = b.getOrigin();
 		Power bPower = b.getPowers();
 
-		ArrayList<Double> cExps = c.getExponents();
-		ArrayList<Double> cCoefs = c.getCoefficients();
-		ArrayList<Double> cNorms = c.getPrimNorms();
+		List<Double> cExps = c.getExponents();
+		List<Double> cCoefs = c.getCoefficients();
+		List<Double> cNorms = c.getPrimNorms();
 		Vector3D cOrigin = c.getOrigin();
 		Power cPower = c.getPowers();
 
-		ArrayList<Double> dExps = d.getExponents();
-		ArrayList<Double> dCoefs = d.getCoefficients();
-		ArrayList<Double> dNorms = d.getPrimNorms();
+		List<Double> dExps = d.getExponents();
+		List<Double> dCoefs = d.getCoefficients();
+		List<Double> dNorms = d.getPrimNorms();
 		Vector3D dOrigin = d.getOrigin();
 		Power dPower = d.getPowers();
 

src/main/java/name/mjw/jquante/math/qm/integral/RysTwoElectronTerm.java
@@ -1,7 +1,7 @@
 package name.mjw.jquante.math.qm.integral;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -56,21 +56,21 @@ public class RysTwoElectronTerm implements TwoElectronTerm {
 		double kcNorm;
 		double repulsionTerm;
 
-		ArrayList<Double> aExps;
-		ArrayList<Double> aCoefs;
-		ArrayList<Double> aNorms;
+		List<Double> aExps;
+		List<Double> aCoefs;
+		List<Double> aNorms;
 
-		ArrayList<Double> bExps;
-		ArrayList<Double> bCoefs;
-		ArrayList<Double> bNorms;
+		List<Double> bExps;
+		List<Double> bCoefs;
+		List<Double> bNorms;
 
-		ArrayList<Double> cExps;
-		ArrayList<Double> cCoefs;
-		ArrayList<Double> cNorms;
+		List<Double> cExps;
+		List<Double> cCoefs;
+		List<Double> cNorms;
 
-		ArrayList<Double> dExps;
-		ArrayList<Double> dCoefs;
-		ArrayList<Double> dNorms;
+		List<Double> dExps;
+		List<Double> dCoefs;
+		List<Double> dNorms;
 
 		aExps = a.getExponents();
 		aCoefs = a.getCoefficients();

commit 482d55c55fd397f095fd158f3861c1a545a6dc45

Attempt to fix performance regression from 78f1de6
02/11/18 OpenJDK 10.0.2+13-1ubuntu0.18.04.3
============================================

void (home)

jquante-jmh	fc63182d

mjw@void:~/workspace/jquante-jmh$ java --version
openjdk 10.0.2 2018-07-17
OpenJDK Runtime Environment (build 10.0.2+13-Ubuntu-1ubuntu0.18.04.3)
OpenJDK 64-Bit Server VM (build 10.0.2+13-Ubuntu-1ubuntu0.18.04.3, mixed mode)

mjw@void:~/workspace/jquante-jmh$ uname -a
Linux void 4.15.0-38-generic #41-Ubuntu SMP Wed Oct 10 10:59:38 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

jquante		20c34663 (master)

Benchmark                             Mode  Cnt    Score    Error  Units
HGPTwoElectronTermBenchmark.sd        avgt   10  154.587 ±  2.907  ns/op
HGPTwoElectronTermBenchmark.sp        avgt   10   69.341 ±  0.961  ns/op
HGPTwoElectronTermBenchmark.ss        avgt   10   20.380 ±  0.164  ns/op
HuzinagaTwoElectronTermBenchmark.sd   avgt   10   13.894 ±  0.076  ns/op
HuzinagaTwoElectronTermBenchmark.sp   avgt   10   13.962 ±  0.105  ns/op
HuzinagaTwoElectronTermBenchmark.ss   avgt   10   13.936 ±  0.096  ns/op
RysTwoElectronTermBenchmark.sd        avgt   10  633.050 ± 10.846  ns/op
RysTwoElectronTermBenchmark.sp        avgt   10  683.754 ±  5.255  ns/op
RysTwoElectronTermBenchmark.ss        avgt   10  686.351 ± 13.072  ns/op
SinglePointHFWater631gdp.doBenchmark  avgt   10    0.429 ±  0.011   s/op
SinglePointHFWaterSTO3G.doBenchmark   avgt   10    0.034 ±  0.001   s/op
SinglePointHFWaterccpvtz.doBenchmark  avgt   10   11.504 ±  0.386   s/op

03/11/18

Benchmark                             Mode  Cnt    Score    Error  Units
HGPTwoElectronTermBenchmark.sd        avgt   10  127.764 ±  0.804  ns/op
HGPTwoElectronTermBenchmark.sp        avgt   10   53.766 ±  0.526  ns/op
HGPTwoElectronTermBenchmark.ss        avgt   10   13.470 ±  0.083  ns/op
HuzinagaTwoElectronTermBenchmark.sd   avgt   10    7.859 ±  0.127  ns/op
HuzinagaTwoElectronTermBenchmark.sp   avgt   10    7.816 ±  0.015  ns/op

src/main/java/name/mjw/jquante/math/qm/basis/ContractedGaussian.java
@@ -30,17 +30,17 @@ public class ContractedGaussian {
 	/**
 	 * The Primitive Gaussians (PGs) that make up this Contracted Gaussian.
 	 */
-	private List<PrimitiveGaussian> primitives;
+	private ArrayList<PrimitiveGaussian> primitives;
 
 	/**
 	 * The list of exponents for this Gaussian.
 	 */
-	private List<Double> exponents;
+	private ArrayList<Double> exponents;
 
 	/**
 	 * Holds value of property coefficients.
 	 */
-	private List<Double> coefficients;
+	private ArrayList<Double> coefficients;
 
 	/**
 	 * Holds value of property normalization.
@@ -52,7 +52,7 @@ public class ContractedGaussian {
 	 * 
 	 * normalization factors for PGs
 	 */
-	private List<Double> primNorms;
+	private ArrayList<Double> primNorms;
 
 	protected Atom centeredAtom;
 
@@ -170,7 +170,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property primitives.
 	 */
-	public List<PrimitiveGaussian> getPrimitives() {
+	public ArrayList<PrimitiveGaussian> getPrimitives() {
 		return this.primitives;
 	}
 
@@ -180,7 +180,7 @@ public class ContractedGaussian {
 	 * @param primitives
 	 *            New value of property primitives.
 	 */
-	public void setPrimitives(List<PrimitiveGaussian> primitives) {
+	public void setPrimitives(ArrayList<PrimitiveGaussian> primitives) {
 		this.primitives = primitives;
 	}
 
@@ -189,7 +189,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property exponents.
 	 */
-	public List<Double> getExponents() {
+	public ArrayList<Double> getExponents() {
 		return this.exponents;
 	}
 
@@ -199,7 +199,7 @@ public class ContractedGaussian {
 	 * @param exponents
 	 *            New value of property exponents.
 	 */
-	public void setExponents(List<Double> exponents) {
+	public void setExponents(ArrayList<Double> exponents) {
 		this.exponents = exponents;
 	}
 
@@ -208,7 +208,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property coefficients.
 	 */
-	public List<Double> getCoefficients() {
+	public ArrayList<Double> getCoefficients() {
 		return this.coefficients;
 	}
 
@@ -218,7 +218,7 @@ public class ContractedGaussian {
 	 * @param coefficients
 	 *            New value of property coefficients.
 	 */
-	public void setCoefficients(List<Double> coefficients) {
+	public void setCoefficients(ArrayList<Double> coefficients) {
 		this.coefficients = coefficients;
 	}
 
@@ -264,7 +264,7 @@ public class ContractedGaussian {
 	public double overlap(ContractedGaussian cg) {
 		double sij = 0.0;
 
-		List<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
+		ArrayList<PrimitiveGaussian> cgPrimitives = cg.getPrimitives();
 
 		for (int i = 0; i < primitives.size(); i++) {
 			PrimitiveGaussian iPG = primitives.get(i);
@@ -715,7 +715,7 @@ public class ContractedGaussian {
 	 * 
 	 * @return Value of property primNorms.
 	 */
-	public List<Double> getPrimNorms() {
+	public ArrayList<Double> getPrimNorms() {
 		return this.primNorms;
 	}
 

src/main/java/name/mjw/jquante/math/qm/integral/HGPTwoElectronTerm.java
@@ -1,5 +1,6 @@
 package name.mjw.jquante.math.qm.integral;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import name.mjw.jquante.math.qm.Density;
@@ -99,11 +100,11 @@ public class HGPTwoElectronTerm implements TwoElectronTerm {
 	 * 
 	 * @return Contribution to Horizontal Recurrence Relation.
 	 */
-	protected double contractedHrr(Vector3D a, Power aPower, List<Double> aCoeff, List<Double> aExps,
-			List<Double> aNorms, Vector3D b, Power bPower, List<Double> bCoeff, List<Double> bExps,
-			List<Double> bNorms, Vector3D c, Power cPower, List<Double> cCoeff, List<Double> cExps,
-			List<Double> cNorms, Vector3D d, Power dPower, List<Double> dCoeff, List<Double> dExps,
-			List<Double> dNorms) {
+	protected double contractedHrr(Vector3D a, Power aPower, ArrayList<Double> aCoeff, ArrayList<Double> aExps,
+			ArrayList<Double> aNorms, Vector3D b, Power bPower, ArrayList<Double> bCoeff, ArrayList<Double> bExps,
+			ArrayList<Double> bNorms, Vector3D c, Power cPower, ArrayList<Double> cCoeff, ArrayList<Double> cExps,
+			ArrayList<Double> cNorms, Vector3D d, Power dPower, ArrayList<Double> dCoeff, ArrayList<Double> dExps,
+			ArrayList<Double> dNorms) {
 
 		int la = aPower.getL();
 		int ma = aPower.getM();
@@ -212,11 +213,11 @@ public class HGPTwoElectronTerm implements TwoElectronTerm {
 	 * 
 	 * @return Contribution to Vertical Recurrence Relation.
 	 */
-	protected double contractedVrr(Vector3D a, Power aPower, List<Double> aCoeff, List<Double> aExps,
-			List<Double> aNorms, Vector3D b, List<Double> bCoeff, List<Double> bExps,
-			List<Double> bNorms, Vector3D c, Power cPower, List<Double> cCoeff, List<Double> cExps,
-			List<Double> cNorms, Vector3D d, List<Double> dCoeff, List<Double> dExps,
-			List<Double> dNorms) {
+	protected double contractedVrr(Vector3D a, Power aPower, ArrayList<Double> aCoeff, ArrayList<Double> aExps,
+			ArrayList<Double> aNorms, Vector3D b, ArrayList<Double> bCoeff, ArrayList<Double> bExps,
+			ArrayList<Double> bNorms, Vector3D c, Power cPower, ArrayList<Double> cCoeff, ArrayList<Double> cExps,
+			ArrayList<Double> cNorms, Vector3D d, ArrayList<Double> dCoeff, ArrayList<Double> dExps,
+			ArrayList<Double> dNorms) {
 
 		double value = 0.0;
 

src/main/java/name/mjw/jquante/math/qm/integral/HuzinagaTwoElectronTerm.java
@@ -1,6 +1,6 @@
 package name.mjw.jquante.math.qm.integral;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -49,27 +49,27 @@ public class HuzinagaTwoElectronTerm implements TwoElectronTerm {
 		double kcNorm;
 		double repulsionTerm;
 
-		List<Double> aExps = a.getExponents();
-		List<Double> aCoefs = a.getCoefficients();
-		List<Double> aNorms = a.getPrimNorms();
+		ArrayList<Double> aExps = a.getExponents();
+		ArrayList<Double> aCoefs = a.getCoefficients();
+		ArrayList<Double> aNorms = a.getPrimNorms();
 		Vector3D aOrigin = a.getOrigin();
 		Power aPower = a.getPowers();
 
-		List<Double> bExps = b.getExponents();
-		List<Double> bCoefs = b.getCoefficients();
-		List<Double> bNorms = b.getPrimNorms();
+		ArrayList<Double> bExps = b.getExponents();
+		ArrayList<Double> bCoefs = b.getCoefficients();
+		ArrayList<Double> bNorms = b.getPrimNorms();
 		Vector3D bOrigin = b.getOrigin();
 		Power bPower = b.getPowers();
 
-		List<Double> cExps = c.getExponents();
-		List<Double> cCoefs = c.getCoefficients();
-		List<Double> cNorms = c.getPrimNorms();
+		ArrayList<Double> cExps = c.getExponents();
+		ArrayList<Double> cCoefs = c.getCoefficients();
+		ArrayList<Double> cNorms = c.getPrimNorms();
 		Vector3D cOrigin = c.getOrigin();
 		Power cPower = c.getPowers();
 
-		List<Double> dExps = d.getExponents();
-		List<Double> dCoefs = d.getCoefficients();
-		List<Double> dNorms = d.getPrimNorms();
+		ArrayList<Double> dExps = d.getExponents();
+		ArrayList<Double> dCoefs = d.getCoefficients();
+		ArrayList<Double> dNorms = d.getPrimNorms();
 		Vector3D dOrigin = d.getOrigin();
 		Power dPower = d.getPowers();
 