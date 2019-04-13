bc3f7bc34d72aea740447285c530d93763999425
Mark Williamson <mjw@mjw.name>
2019-04-13 06:55:04 +0100

    Fix extra 1e test

--- src/test/java/name/mjw/jquante/math/qm/integral/OneElectronIntegralsTest.java
+++ src/test/java/name/mjw/jquante/math/qm/integral/OneElectronIntegralsTest.java
@@ -48,7 +48,6 @@ class OneElectronIntegralsTest {
 
 	@Test
 	void testgetHCore() {
-		System.out.println(e1.getHCore());
 		assertEquals(-32.70775590590571, e1.getHCore().getEntry(0, 0), diff);
 		assertEquals(-1.7016641597, e1.getHCore().getEntry(6, 0), diff);
 		assertEquals(-5.0309028634, e1.getHCore().getEntry(6, 6), diff);
