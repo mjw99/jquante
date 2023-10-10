448c31cda480c8e0f29e71fb691f5fcbcf292a1f
Mark Williamson <mjw@mjw.name>
2023-10-10 04:32:52 +0100

    Factor out to its own class

--- src/main/java/name/mjw/jquante/math/qm/MolecularOrbitals.java
+++ src/main/java/name/mjw/jquante/math/qm/MolecularOrbitals.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import name.mjw.jquante.math.MathUtil;
+import name.mjw.jquante.math.qm.SortedEigenDecomposition;
 
 /**
  * Represents the Molecular orbitals as a coefficient matrix and the
@@ -100,70 +101,6 @@ public final class MolecularOrbitals extends Array2DRowRealMatrix {
 	@Override
 	public String toString() {
 		return MathUtil.matrixToString(this);
-	}
-
-}
-
-/**
- * Reorders commons-math's EigenDecomposition eigensystem, sorting by the
- * smallest eigenvalue first.
- *
- * @author mjw
- *
- */
-class SortedEigenDecomposition {
-
-	double[] realEigenvalues;
-	RealVector[] eigenvectors;
-
-	final int n;
-
-	public SortedEigenDecomposition(final EigenDecomposition eig) {
-		n = eig.getRealEigenvalues().length;
-
-		realEigenvalues = eig.getRealEigenvalues();
-		eigenvectors = new ArrayRealVector[n];
-
-		for (int k = 0; k < n; ++k) {
-			eigenvectors[k] = eig.getEigenvector(k);
-		}
-
-		RealVector tmpvector;
-		for (int i = 0; i < n; i++) {
-
-			int k = i;
-			double p = realEigenvalues[i];
-
-			for (int j = i + 1; j < n; j++) {
-				if (realEigenvalues[j] < p) {
-					k = j;
-					p = realEigenvalues[j];
-				}
-			}
-
-			if (k != i) {
-				realEigenvalues[k] = realEigenvalues[i];
-				realEigenvalues[i] = p;
-
-				tmpvector = eigenvectors[i];
-				eigenvectors[i] = eigenvectors[k];
-				eigenvectors[k] = tmpvector;
-			}
-		}
-
-	}
-
-	RealMatrix getVT() {
-		RealMatrix vt = MatrixUtils.createRealMatrix(n, n);
-		for (int k = 0; k < n; ++k) {
-			vt.setRowVector(k, eigenvectors[k]);
-		}
-
-		return vt;
-	}
-
-	double[] getRealEigenvalues() {
-		return realEigenvalues;
 	}
 
 }
