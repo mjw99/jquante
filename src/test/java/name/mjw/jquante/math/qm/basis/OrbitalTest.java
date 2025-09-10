e662841ae521c2bbf3ee1a80fddec93fefcb7903
Mark Williamson <mjw@mjw.name>
2025-09-10 11:54:59 +0100

    Add more tests for Orbital

--- src/test/java/name/mjw/jquante/math/qm/basis/OrbitalTest.java
+++ src/test/java/name/mjw/jquante/math/qm/basis/OrbitalTest.java
@@ -10,11 +10,18 @@ public class OrbitalTest {
 	private final double delta = 0.000001;
 	private static Orbital orbital;
 
+	private static Orbital largerOrbital;
+
 	@BeforeAll
 	public static void setUp() {
 		orbital = new Orbital("S");
 		orbital.addCoefficient(1.0);
 		orbital.addExponent(2.0);
+
+		largerOrbital = new Orbital("P");
+		largerOrbital.addEntry(1.0, 2.0);
+		largerOrbital.addEntry(3.0, 4.0);
+
 	}
 
 	@Test
@@ -25,13 +32,27 @@ public class OrbitalTest {
 
 	@Test
 	void testGetCoefficients() {
+		assertEquals(1, orbital.getCoefficients().size());
 		assertEquals(1.0, orbital.getCoefficients().get(0), delta);
 
 	}
 
 	@Test
 	void testGetExponent() {
+		assertEquals(1, orbital.getExponents().size());
 		assertEquals(2.0, orbital.getExponents().get(0), delta);
+
+	}
+
+	@Test
+	void testAddEntry() {
+		assertEquals(2, largerOrbital.getCoefficients().size());
+
+		assertEquals(1.0, largerOrbital.getCoefficients().get(0), delta);
+		assertEquals(2.0, largerOrbital.getExponents().get(0), delta);
+
+		assertEquals(3.0, largerOrbital.getCoefficients().get(1), delta);
+		assertEquals(4.0, largerOrbital.getExponents().get(1), delta);
 
 	}
 
